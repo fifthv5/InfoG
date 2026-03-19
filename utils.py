f14e1b8d92cade445eb5823567232457ac68fab4
Chargogagoman <fifth@wejiv.net>
2026-03-19 09:58:18 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def toolname():
     |%%%| |%%%| \%%\ |%%%| |%%%|       |%%|______|%%| |@@@@@@@@@@@@@@|
     |___| |___|  \_______| |___|       |____________| |______________|By: Fifthv5
     For more visit: https://github/fifthv5
-{Gr}'''
+{Gr}''')
 
 def domain_name():
     print(socket.gethostbyname(input('Domain name: ')))
@@ -34,22 +34,22 @@ def domain_name():
 
 def your_ip():
     your_ip=requests.get('https://geolocation-db.com/json/').json() 
-    print('Your public IP address: ',your_ip.get('IPv4'))
+    print('   Your public IP address: ',your_ip.get('IPv4'))
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    print('Your device name: ',hostname)
-    print('Your private IP address ',ip_address)
+    print('   Your device name: ',hostname)
+    print('   Your private IP address ',ip_address)
 
 
 def ip_lookup():
-    enter_ip = input('IP Address: ')
+    enter_ip = input('   IP Address: ')
     response = requests.get(f"https://geolocation-db.com/json/{enter_ip}&position=true").json()
     for key, value in response.items():
         print(key, ' : ', value)
 
 
 def ph_num_lookup():
-    your_ph_num = input('Your Number: ')
+    your_ph_num = input('  Your Number: ')
     phone_number = phonenumbers.parse(your_ph_num)
     # put your phonenumber with the country code eg:+1xxxxxxxx
     print(geocoder.description_for_number(phone_number,
@@ -65,7 +65,7 @@ def ph_num_lookup():
     
 def ip_grabber():
     if platform.system() == 'Windows':
-        print('IP-Grabber is not supported on Windows')
+        print('   IP-Grabber is not supported on Windows')
     else:
         os.system('cd IP-Grabber && sudo chmod 777 * && bash ip-grabber.sh')
 def clear():
